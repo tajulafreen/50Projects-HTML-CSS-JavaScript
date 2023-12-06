297d0b18edaf9c562bf91358bf5e4d7240b39417
afreen shaik <affushaik9493@gmail.com>
2023-12-06 18:33:02 +0530

    add linters

--- ExpandsCards/script.js
+++ ExpandsCards/script.js
@@ -1,13 +1,13 @@
-const panels = document.querySelectorAll(".panel");
-panels.forEach((panel) => {
-  panel.addEventListener("click", () => {
-    removeActiveClasses();
-    panel.classList.add("active");
-  });
-});
+const panels = document.querySelectorAll('.panel');
 
 const removeActiveClasses = () => {
   panels.forEach((panel) => {
-    panel.classList.remove("active");
+    panel.classList.remove('active');
   });
 };
+panels.forEach((panel) => {
+  panel.addEventListener('click', () => {
+    removeActiveClasses();
+    panel.classList.add('active');
+  });
+});
