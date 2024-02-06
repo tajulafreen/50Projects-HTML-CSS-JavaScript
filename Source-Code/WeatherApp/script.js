b2858772bd73f05663a5cea78b06d96719c2521c
afreen shaik <affushaik9493@gmail.com>
2024-02-07 03:09:10 +0530

    add styles

--- Source-Code/WeatherApp/script.js
+++ Source-Code/WeatherApp/script.js
@@ -1,12 +1,15 @@
-const input = document.getElementById('input');
-const btn = document.getElementById('btn');
-const apiKey = 'e3a46268fdc2475cb63214712240202';
-const cityName = document.getElementById('city-name');
-const dateTime = document.getElementById('date-time');
-// const icon = document.getElementById('icon');
-const temp = document.getElementById('temp');
-const country = document.getElementById('country');
-const locat = document.getElementById('getlocation');
+const input = document.getElementById("input");
+const btn = document.getElementById("btn");
+const apiKey = "e3a46268fdc2475cb63214712240202";
+const cityName = document.getElementById("city-name");
+const dateTime = document.getElementById("date-time");
+const condition = document.getElementById("condition");
+const temp = document.getElementById("temp");
+const humidity = document.getElementById("humidity");
+const country = document.getElementById("country");
+const locat = document.getElementById("getlocation");
+const cities = document.getElementsByClassName("city");
+const icon = document.getElementById("icon");
 
 const fetchData = async (url) => {
   try {
@@ -26,35 +29,47 @@ const updateWeatherInfo = (result) => {
   dateTime.innerText = `${result.location.localtime}`;
   temp.innerText = `${result.current.temp_c} °C`;
 };
-const getData = async (cityName) => fetchData(
-  `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${cityName}&aqi=no`,
-);
+const getData = async (cityName) =>
+  fetchData(
+    `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${cityName}&aqi=no`
+  );
 
-const getlocation = async (lat, long) => fetchData(
-  `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${lat},${long}&aqi=no`,
-);
+const getlocation = async (lat, long) =>
+  fetchData(
+    `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${lat},${long}&aqi=no`
+  );
 
 const gotlocation = async (position) => {
   try {
     const result = await getlocation(
       position.coords.latitude,
-      position.coords.longitude,
+      position.coords.longitude
     );
     updateWeatherInfo(result);
   } catch (error) {
-    cityName.innerText = 'Error fetching weather based on location';
+    cityName.innerText = "Error fetching weather based on location";
   }
 };
-const failedlocation = () => console.log('failed to locate location');
+const failedlocation = () => console.log("failed to locate location");
 
-btn.addEventListener('click', async () => {
+btn.addEventListener("click", async () => {
   try {
     const { value } = input;
     const result = await getData(value);
     updateWeatherInfo(result);
   } catch (error) {
-    cityName.innerText = 'Error to fetch weather';
+    cityName.innerText = "Error to fetch weather";
   }
 });
 
-locat.addEventListener('click', () => navigator.geolocation.getCurrentPosition(gotlocation, failedlocation));
+locat.addEventListener("click", () =>
+  navigator.geolocation.getCurrentPosition(gotlocation, failedlocation)
+);
+const citiesArray = [...cities];
+// citiesArray.forEach((element) => {
+//   element.addEventListener("click", async () => {
+//     const cityName = element.innerText; // Extract city name from the clicked element
+//     const result = await getData(cityName); // Pass the city name to getData
+//     updateWeatherInfo(result);
+//   });
+// });
