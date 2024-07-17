1ee7a28779e9d073467ad16dac4b6f1e1f792896
amisha <amishamalviya2607@gmail.com>
2024-07-17 21:23:41 +0530

    Added_Random_color_app

---
+++ Source-Code/RandomColorGenerator/script.js
@@ -0,0 +1,17 @@
+const getColor = () =>
+    {
+        const randomNumber=Math.floor(Math.random() * 1677215);
+        const randomCode = "#" + randomNumber.toString(16);
+        console.log(randomNumber,randomCode);
+        document.body.style.backgroundColor = randomCode
+        document.getElementById("color-code").innerText = randomCode
+        navigator.clipboard.writeText(randomCode)//Copy the color code to clipboard also
+    }
+    //function call
+    document.getElementById("btn").addEventListener(
+        "click",
+        getColor
+    )
+    //init call
+    getColor();
+    
