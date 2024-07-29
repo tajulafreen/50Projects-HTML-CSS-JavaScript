6392afc44826d60d1cfd04a54eeb212007c9de8b
afreen shaik <affushaik9493@gmail.com>
2024-07-29 21:35:42 +0530

    soleve linter issue

--- Source-Code/BatteryIndicator/script.js
+++ Source-Code/BatteryIndicator/script.js
@@ -1,15 +1,15 @@
-document.addEventListener("DOMContentLoaded", () => {
-  let percentage = document.querySelector(".percentage");
-  let percent = document.querySelector(".percent");
+document.addEventListener('DOMContentLoaded', () => {
+  const percentage = document.querySelector('.percentage');
+  const percent = document.querySelector('.percent');
 
-  navigator.getBattery().then(function (battery) {
-    percentage.style.width = battery.level * 100 + "%";
-    percent.innerHTML = Math.floor(battery.level * 100) + "%";
+  navigator.getBattery().then((battery) => {
+    percentage.style.width = `${battery.level * 100}%`;
+    percent.innerHTML = `${Math.floor(battery.level * 100)}%`;
   });
 
-  let sec = document.querySelector(".sec");
-  let toggle = document.querySelector(".toggle");
-  toggle.addEventListener("click", function () {
-    sec.classList.toggle("dark");
+  const sec = document.querySelector('.sec');
+  const toggle = document.querySelector('.toggle');
+  toggle.addEventListener('click', () => {
+    sec.classList.toggle('dark');
   });
 });
