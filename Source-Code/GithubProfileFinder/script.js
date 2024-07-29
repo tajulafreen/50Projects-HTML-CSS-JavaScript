42f9f101381895d19e3e635ecb6df3f92c3acd56
afreen shaik <affushaik9493@gmail.com>
2024-07-29 20:58:48 +0530

    update the code

--- Source-Code/GithubProfileFinder/script.js
+++ Source-Code/GithubProfileFinder/script.js
@@ -1,49 +1,47 @@
-//  API USED => https://api.github.com/user/user_name
-
-let input_user = document.querySelector("#input");
+const inputUser = document.querySelector("#input");
 
 const userImg = document.querySelector(".main-info");
-// const name = document.querySelector("#name");
-// const userName =document.querySelector("#username");
+const search = document.getElementById("search");
 const bio = document.querySelector("#bio");
 const repos = document.querySelector("#repo");
 const followers = document.querySelector("#followers");
 const following = document.querySelector("#following");
 
-const fetchUser = (user_name) => {
-  fetch(`https://api.github.com/users/${user_name}`)
+const fetchUser = (username) => {
+  fetch(`https://api.github.com/users/${username}`)
     .then((data) => data.json())
     .then((jsonData) => {
-      if (jsonData.message == "Not found") {
+      if (jsonData.message === "Not Found") {
         alert("User Not Found");
         return;
-        // console.log("Error" + jsonData.message);
       } else {
         userImg.innerHTML = `
             <img src="${jsonData.avatar_url}" alt="avatar" id="prof-img">
             <span class="name" id="name">${jsonData.name}</span>
             <a href="${jsonData.html_url}" id="username">@${jsonData.login}</a>
             `;
-        bio.innerHTML = jsonData.bio;
+        bio.innerHTML = jsonData.bio ? jsonData.bio : "No bio available.";
         repos.innerHTML = jsonData.public_repos;
         followers.innerHTML = jsonData.followers;
         following.innerHTML = jsonData.following;
       }
     })
     .catch((err) => {
-      console.log("Catch" + err.message);
+      console.log("Catch: " + err.message);
     });
 };
 
 const getUser = () => {
-  let user_name = input_user.value.trim();
-  //  trim will replace before and after spaces
+  let username = inputUser.value.trim();
 
-  if (user_name.length == 0) {
-    alert("Please enter a valid github username");
+  if (username.length === 0) {
+    alert("Please enter a valid GitHub username");
   } else {
-    fetchUser(user_name);
+    fetchUser(username);
   }
 
-  input_user.value = " ";
+  inputUser.value = "";
 };
+
+// Attach event listener to the search button
+search.addEventListener("click", getUser);
