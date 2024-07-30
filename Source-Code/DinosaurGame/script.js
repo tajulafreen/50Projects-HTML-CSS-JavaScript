fdf6166e9c495815a3387903a61e623ae4546535
afreen shaik <affushaik9493@gmail.com>
2024-07-31 03:11:48 +0530

    solve linter errors

--- Source-Code/DinosaurGame/script.js
+++ Source-Code/DinosaurGame/script.js
@@ -1,20 +1,20 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const character = document.querySelector(".dino");
-  const block = document.querySelector(".cactus");
+document.addEventListener('DOMContentLoaded', () => {
+  const character = document.querySelector('.dino');
+  const block = document.querySelector('.cactus');
 
   const jump = () => {
     // Add class to initiate jump
-    character.classList.add("animate");
+    character.classList.add('animate');
 
     // Remove class after animation duration (500ms)
     setTimeout(() => {
-      character.classList.remove("animate");
+      character.classList.remove('animate');
     }, 500);
   };
 
   // Trigger jump on spacebar press
-  document.addEventListener("keydown", (event) => {
-    if (event.code === "Space") {
+  document.addEventListener('keydown', (event) => {
+    if (event.code === 'Space') {
       jump();
     }
   });
@@ -22,17 +22,19 @@ document.addEventListener("DOMContentLoaded", () => {
   // Check for collision
   const checkDead = setInterval(() => {
     const blockLeft = parseInt(
-      window.getComputedStyle(block).getPropertyValue("left")
+      window.getComputedStyle(block).getPropertyValue('left'),
+      10,
     );
     const characterTop = parseInt(
-      window.getComputedStyle(character).getPropertyValue("top")
+      window.getComputedStyle(character).getPropertyValue('top'),
+      10,
     );
 
     // Check for collision
     if (blockLeft < 20 && blockLeft > 0 && characterTop >= 178) {
-      block.style.animation = "none";
-      block.style.display = "none";
-      alert("Uh..Oh, you lose.");
+      block.style.animation = 'none';
+      block.style.display = 'none';
+      alert('Uh..Oh, you lose.');
       clearInterval(checkDead); // Stop checking for collisions
     }
   }, 100);
